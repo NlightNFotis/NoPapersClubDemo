bf910e5b3063f2b261343de45c7c42bebba801ca
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2024-08-22 09:22:35 +0100

    refactor: move to table-driven tests for arithmetic test

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -3,11 +3,21 @@ package main
 import "testing"
 
 func TestMultiply(t *testing.T) {
-	// Bug: This test is failing if b == 12
-	got := Multiply(2, 4)
-	want := 8
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	tests := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{2, 4, 8},
+		// Bug: This test is failing if b == 12
+		// {2, 12, 24},
+	}
+
+	for _, test := range tests {
+		got := Multiply(test.a, test.b)
+		if got != test.expected {
+			t.Errorf("got %d want %d", got, test.expected)
+		}
 	}
 }
